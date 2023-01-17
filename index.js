ea0f3c8b3c74dc8f01759c9531feba2d11846777
Vitor Castro <81660618+vitorcastro018@users.noreply.github.com>
2023-01-17 18:13:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function wpp1(){
+    location.href = 'https://wa.me/5564981432153?text=Ol%C3%A1%2C+Gostaria+de+saber+mais+sobre+os+cursos'
+}
+
+function wpp2(){
+    location.href = 'https://wa.me/5564992998066?text=Ol%C3%A1%2C+Gostaria+de+saber+mais+sobre+os+cursos'
+}
+
+function site(){
+    location.href = 'https://www.admcursosetreinamentos.com.br'
+}
